175cae8946af1298e670d7d9b10e3672250016fc
kullu-ashish <kullu.ashish@gmail.com>
2020-08-22 16:55:48 +1000

    Create PrintCommandLineArguments.java

---
+++ PrintCommandLineArguments.java
@@ -0,0 +1,15 @@
+public class PrintCommandLineArguments {
+
+      public static void main(String[] args) {
+
+            System.out.println("Command-line arguments:");
+
+            for (String arg : args) {
+
+                  System.out.println(arg);
+
+            }
+
+      }
+
+}
