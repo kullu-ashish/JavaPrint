0b000671c8a83bf2dc5d5281e09cadd7d1cec8b8
kullu-ashish <kullu.ashish@gmail.com>
2020-08-26 11:07:17 +1000

    Update runJavaProgram.sh

--- runJavaProgram.sh
+++ runJavaProgram.sh
@@ -6,7 +6,7 @@ echo "This is my simple test job!."
 echo "jobId: $AWS_BATCH_JOB_ID"
 echo "jobQueue: $AWS_BATCH_JQ_NAME"
 echo "computeEnvironment: $AWS_BATCH_CE_NAME"
-aws s3 s3://$1/$2 /tmp
+aws s3 cp s3://$1/$2 /tmp
 java -jar sample.jar `cat /tmp/$2`
 date
 echo "bye bye!!"
