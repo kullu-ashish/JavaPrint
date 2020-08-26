0d025c182e9823be9f4360a157f64d8a57e049ca
kullu-ashish <kullu.ashish@gmail.com>
2020-08-26 10:03:05 +1000

    Create runJavaProgram.sh

---
+++ runJavaProgram.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+date
+echo "Args: $@"
+env
+echo "This is my simple test job!."
+echo "jobId: $AWS_BATCH_JOB_ID"
+echo "jobQueue: $AWS_BATCH_JQ_NAME"
+echo "computeEnvironment: $AWS_BATCH_CE_NAME"
+aws s3 s3://$1/$2 /tmp
+java -jar sample.jar `cat /tmp/$2`
+date
+echo "bye bye!!"
